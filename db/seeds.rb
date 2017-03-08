ffd11fee36ecd741e497febdf7d975f599c56205
jonathan parisot <parisot.jonathan@gmail.com>
2017-03-08 13:09:37 +0100

    Built the backend, some work to do on the frontend for it to look nice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_array = [
+  {
+    name: "Tour d'argent",
+    address: "Sur les quais",
+    category: "french"
+  },
+  {
+    name: "McDonards",
+    address: "Partour",
+    category: "belgian"
+  },
+  {
+    name: "Mediterraneo",
+    address: "9west",
+    category: "italian"
+  },
+  {
+    name: "Tian Huang",
+    address: "14, rue bichat",
+    category: "chinese"
+  },
+  {
+    name: "Sushi shop",
+    address: "dans le 5e",
+    category: "japanese"
+  },
+
+]
+
+restaurants_array.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
